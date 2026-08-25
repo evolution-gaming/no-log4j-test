ed6951962c2a6110410838f9601e5783735e4198
Mareks Rampāns <8796159+mr-git@users.noreply.github.com>
2026-08-25 15:04:11 +0300

    Update scala-github-actions workflows to v7.0.1

--- src/main/scala/com/evolutiongaming/util/NoLog4jSuite.scala
+++ src/main/scala/com/evolutiongaming/util/NoLog4jSuite.scala
@@ -13,9 +13,9 @@ trait NoLog4jSuite extends AnyFunSuite with Matchers {
 
 object Log4jDeps {
   def apply(): Map[String, String] = Map(
-    ("log4j"          , "org.apache.log4j.Dispatcher"),
+    ("log4j", "org.apache.log4j.Dispatcher"),
     ("commons-logging", "org.apache.commons.logging.LogSource"),
-    ("slf4j-log4j12"  , "org.slf4j.impl.Log4jLoggerAdapter"),
-    ("log4j2"         , "org.apache.logging.log4j.core.appender.AbstractAppender")
+    ("slf4j-log4j12", "org.slf4j.impl.Log4jLoggerAdapter"),
+    ("log4j2", "org.apache.logging.log4j.core.appender.AbstractAppender"),
   )
 }
